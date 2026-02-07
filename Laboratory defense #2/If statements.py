9b3c9b36a4b1ed07b7a943db88502e96762b428d
managersultan <s_bagitov@kbtu.kz>
2026-02-07 14:35:58 +0500

    Update If statements.py

--- Laboratory defense #2/If statements.py
+++ Laboratory defense #2/If statements.py
@@ -1,9 +1,9 @@
 a = 33
 b = 200
 if b > a:
-  print("b is greater than a")
+  print("b is greater than a") #b is greater than a"
 
 
 number = 15
 if number > 0:
-  print("The number is positive")
+  print("The number is positive") # The number is positive
