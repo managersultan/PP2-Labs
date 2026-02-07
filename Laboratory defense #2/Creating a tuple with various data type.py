bc25b47736963496ad5235a73e3264397074363a
managersultan <s_bagitov@kbtu.kz>
2026-02-07 14:37:33 +0500

    Update Creating a tuple with various data type.py

--- Laboratory defense #2/Creating a tuple with various data type.py
+++ Laboratory defense #2/Creating a tuple with various data type.py
@@ -4,11 +4,11 @@ print(tup)
 tup1 = (0, 1, 2, 3)
 tup2 = ('python', 'geek')
 tup3 = (tup1, tup2)
-print(tup3)
+print(tup3) # ((0, 1, 2, 3), ('python', 'geek'))
 
 # Creating a Tuple with repetition
 tup1 = ('Geeks',) * 3
-print(tup1)
+print(tup1) # ('Geeks', 'Geeks', 'Geeks')
 
 # Creating a Tuple with the use of loop
 tup = ('Geeks')
