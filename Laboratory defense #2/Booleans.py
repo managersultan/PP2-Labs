f22bea73a993f68a0ebc0f28fe4cdc8d8246940d
managersultan <s_bagitov@kbtu.kz>
2026-02-07 14:35:18 +0500

    Update Booleans.py

--- Laboratory defense #2/Booleans.py
+++ Laboratory defense #2/Booleans.py
@@ -7,6 +7,6 @@ a = 200
 b = 33
 
 if b > a:
-  print("b is greater than a")
+  print("b is greater than a") #False. 
 else:
-  print("b is not greater than a")
+  print("b is not greater than a") # b is not greater than a
